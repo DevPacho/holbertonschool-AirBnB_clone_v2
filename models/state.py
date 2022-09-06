88fb56172fc06f78ef0d025bcef92aac25a0056e
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-06 10:01:57 -0500

    update state class - add proprety to use just in filestorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
+import models
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+import os
 
 
 class State(BaseModel, Base):
@@ -11,3 +14,17 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="delete", backref="state")
+
+
+if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    # with state_id equals to the current State.id
+    # just for filestore returns the list of City instances
+    # @property, puede ser usado para modificar un atributo o propiedad.
+    @property
+    def cities(self):
+        """Returns the list of City instances"""
+        new_list = []
+        for city in list(models.storage.all(City).values()):
+            if city.state_id == self.id:
+                new_list.append(city)
+        return new_list
