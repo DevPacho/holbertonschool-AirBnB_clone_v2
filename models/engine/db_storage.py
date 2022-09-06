9f9f4cd74a09b4b2adb1e32514c61360b6bfdddb
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-09-05 23:08:35 -0500

    update db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from os import getenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.city import City
@@ -25,3 +26,29 @@ class DBStorage:
 
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """List current database session"""
+        pass
+
+    def new(self, obj):
+        """Adds the object to the current session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commits all changes"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Deletes from the db session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reload all tables in the database"""
+
+        Base.metadata.create_all(self.__engine)
+        my_session = sessionmaker(bind=self.__engine,
+                                  expire_on_commit=False)
+        Session = scoped_session(my_session)
+        self.__session = Session()
