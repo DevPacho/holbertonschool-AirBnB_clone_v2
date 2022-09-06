5bff0f9ab77ad91bafffc912fd1a316d94ea318d
DevPacho <frankjose2004@gmail.com>
2022-09-06 14:57:32 -0500

    Trying to solve some errors

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
+        # cities = relationship("City", cascade="all, delete", backref="state")
 
     else:
         name = ""
