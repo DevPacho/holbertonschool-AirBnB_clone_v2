fd5c689fa60e85c69c085d459cce736aab65092b
DevPacho <frankjose2004@gmail.com>
2022-09-06 20:04:41 -0500

    [CHANGED] trying with relationships

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -12,7 +13,7 @@ class City(BaseModel, Base):
         
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        # places = relationship("Place", cascade="all, delete", backref="cities")
+        places = relationship("Place", cascade="all, delete", backref="cities")
 
     else:
         name = ""
--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
+from models.review import Review
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -14,10 +16,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-        # places = relationship("Place", cascade='all, delete, delete-orphan',
-        #                  backref="user")
-        # reviews = relationship("Review", cascade='all, delete, delete-orphan',
-        #                   backref="user")
+        places = relationship("Place", cascade='all, delete', backref="user")
+        # reviews = relationship("Review", cascade='all, delete', backref="user")
     else:
         email = ''
         password = ''
