361fe2f452ac5f9f5361c414548d73597d26120f
DevPacho <frankjose2004@gmail.com>
2022-09-07 20:32:29 -0500

    [FIXED] all errors when running tests with the database engine

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,6 +8,7 @@ import datetime
 from uuid import UUID
 import json
 import os
+from os import getenv
 
 
 class test_basemodel(unittest.TestCase):
@@ -49,6 +50,7 @@ class test_basemodel(unittest.TestCase):
         with self.assertRaises(TypeError):
             new = BaseModel(**copy)
 
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db", "Engine=FileStorage")
     def test_save(self):
         """ Testing save """
         i = self.value()
