fd43195cdffb745e82a15537864324efd360a913
DevPacho <frankjose2004@gmail.com>
2022-09-14 20:26:16 -0500

    Trying to solve 'Session' error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage database storage for hbnb clone"""
+"""Module that defines the new engine 'DBStorage' for MySQL"""
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
@@ -13,49 +13,58 @@ from models.user import User
 
 
 class DBStorage:
-    """Class DBStorage"""
+    """Engine for connect to MySQL"""
+
     __engine = None
     __session = None
 
     def __init__(self):
-        """Instantiates a new database"""
+        """Instantiation of the database"""
         self.__engine = create_engine(
             "mysql+mysqldb://{}:{}@{}/{}".format(
                 getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'),
                 getenv('HBNB_MYSQL_HOST'), getenv('HBNB_MYSQL_DB')),
             pool_pre_ping=True)
 
-        if getenv('HBNB_ENV') == "test":
+        if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Return a dictionary all cls in DB o all obj un DB"""
+        """Get all objects of a class or all the classes"""
+
         objects = {}
+
         if cls:
-            for obj in self.__session.query(cls).all():
-                objects[obj.to_dict()['__class__'] + '.' + obj.id] = obj
+            query = self.__session.query(cls).all()
+
+            for object in query:
+                objects[f"{object.__class__}.{object.id}"] = object
+
         else:
             classes = [State, Place, User, City, Review, Amenity]
-            for every_class in classes:
-                for ob in self.__session.query(every_class).all():
-                    objects[ob.to_dict()['__class__'] + '.' + ob.id] = ob
+            for input_class in classes:
+                query = self.__session.query(input_class).all()
+
+                for object in query:
+                    objects[f"{object.__class__}.{object.id}"] = object
         return objects
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """Adds the object to the current session"""
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """Commits all changes"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session"""
+        """Deletes from the db session obj if not None"""
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """create all tables in the database"""
+        """Reload all tables in the database"""
+
         Base.metadata.create_all(self.__engine)
         my_session = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
@@ -63,5 +72,5 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call 'remove()' method on the private session attribute"""
         self.__session.close()
